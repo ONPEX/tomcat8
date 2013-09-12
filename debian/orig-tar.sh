000685f4a1fdbafaf70ccec832ffc9e8be0995cc
Emmanuel Bourg <ebourg@apache.org>
2013-09-12 16:29:03 +0200

    Tweaked debian/orig-tar.sh to work with release candidates

--- debian/orig-tar.sh
+++ debian/orig-tar.sh
@@ -3,7 +3,7 @@
 VERSION=$2
 TAR=../tomcat8_$VERSION.orig.tar.xz
 DIR=tomcat8-$VERSION
-TAG=$(echo TOMCAT_$VERSION | sed -e 's/\./_/g')
+TAG=$(echo TOMCAT_$VERSION | sed -e 's/[\.~]/_/g')
 
 svn export http://svn.apache.org/repos/asf/tomcat/tags/$TAG $DIR
 tar -c -J -f $TAR --exclude 'standard.jar' --exclude 'jstl.jar' $DIR
