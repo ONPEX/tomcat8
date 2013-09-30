2b006ee0d6feac2e801a08aaf6b185707d5820a2
Emmanuel Bourg <ebourg@apache.org>
2013-09-30 20:27:16 +0200

    Changed debian/watch to match only the official releases

--- debian/orig-tar.sh
+++ debian/orig-tar.sh
@@ -7,7 +7,7 @@ TAG=$(echo TOMCAT_$VERSION | sed -e 's/[\.~]/_/g')
 
 svn export http://svn.apache.org/repos/asf/tomcat/tags/$TAG $DIR
 tar -c -J -f $TAR --exclude 'standard.jar' --exclude 'jstl.jar' $DIR
-rm -rf $DIR ../$TAG
+rm -rf $DIR ../$TAG $3
 
 # move to directory 'tarballs'
 if [ -r .svn/deb-layout ]; then
