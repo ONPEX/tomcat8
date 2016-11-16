287a102d247fca91a9fd555b177f176448818ade
Emmanuel Bourg <ebourg@apache.org>
2016-11-16 18:45:26 +0100

    Adapted debian/orig-tar.sh to download the 8.5.x releases

--- debian/orig-tar.sh
+++ debian/orig-tar.sh
@@ -5,14 +5,6 @@ TAR=../tomcat8_$VERSION.orig.tar.xz
 DIR=tomcat8-$VERSION
 TAG=$(echo TOMCAT_$VERSION | sed -e 's/[\.~]/_/g')
 
-svn export http://svn.apache.org/repos/asf/tomcat/tc8.0.x/tags/$TAG $DIR
+svn export http://svn.apache.org/repos/asf/tomcat/tc8.5.x/tags/$TAG $DIR
 tar -c -J -f $TAR --exclude 'taglibs-standard-*.jar' $DIR
 rm -rf $DIR ../$TAG $3
-
-# move to directory 'tarballs'
-if [ -r .svn/deb-layout ]; then
-  . .svn/deb-layout
-  mv $TAR $origDir
-  echo "moved $TAR to $origDir"
-fi
-
